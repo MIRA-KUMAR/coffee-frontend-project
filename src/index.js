5b84f4bfc1120f755ed924664cdc8ade8ff9e734
MIRA-KUMAR <mira.kumar.24@gmail.com>
2022-12-04 14:46:43 -0700

    Updating react.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { App } from "./App";
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            name:''
-        }
-        this.onChange = this.onChange.bind(this);
+function App() {
+    const [name, setName] = React.useState('');
+    const [age, setAge] = React.useState('0');
+    function onChange(e){
+        setName(e.target.value);
+    } 
+    function onAgeChange(e){
+        setAge(e.target.value);
     }
-    onChange(event){
-        this.setState({name: event.target.value});
-    }
-    render() {
-        return (
-            <div>
-                <h1>Hello World!</h1> 
-                <h1>{this.state.name}</h1>
-                <input onChange={this.onChange} placeholder="Enter Name" />
-            </div>
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <h1>{ name } - { age }</h1>
+            <input onChange={onChange} placeholder="Enter Name"/>
+            <input onChange={onAgeChange} placeholder="Enter Age" />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+// class App extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             name:''
+//         }
+//         this.onChange = this.onChange.bind(this);
+//     }
+//     onChange(event){
+//         this.setState({name: event.target.value});
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Hello World!</h1> 
+//                 <h1>{this.state.name}</h1>
+//                 <input onChange={this.onChange} placeholder="Enter Name" />
+//             </div>
             
 
-        )
-    }
-}
-// const app = document.getElementById("root");
-ReactDOM.render(<App />, document.getElementById("root"));
+//         )
+//     }
+// }
+// ReactDOM.render(<App />, document.getElementById("root"));
 
 
 // import React from 'react';
